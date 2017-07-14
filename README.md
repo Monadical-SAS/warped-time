044bc4b0c5d8dc82f463a4f35dc61c6da19a34fe
Nick Sweeting <git@nicksweeting.com>
2017-07-14 13:11:05 -0500

    add server-time controls to warped-time

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Simple Javascript library that provides an equivalent to `Date.getTime()` with variable progression speed.
 
+It has a hook to receive timestamps and estimated latency, which it uses to stay in sync with a backend server's time.
+
 **[LIVE DEMO](https://monadical-sas.github.io/redux-time/warped-time/examples/demo.html)**
 
 ```bash
-yarn add warped-time
+yarn add warped-time    # ⏱
 ```
 
 For more details and usage examples see docs on [`redux-time`](https://monadical-sas.github.io/redux-time/).
@@ -13,7 +15,8 @@ For more details and usage examples see docs on [`redux-time`](https://monadical
 ## API
 
 ```javascript
-window.time = new WarpedTime()
+window.time = new WarpedTime(window.store, 1)
+// takes: optional redux store, initial_speed, initial_timestamp
 
 window.time.getActualTime()
 > 1499014500
@@ -31,6 +34,11 @@ window.time.setSpeed(0.01)  // make time progress at 0.01x its actual speed
 
 window.time.getWarpedTime()
 > 1499014499.01
+
+const server_time = timestamp_from_server + rtlatency/2
+window.time.setWarpedTime(server_time) // set time to the server time instantly
+
+window.time.setWarpedTime(server_time, 1000) // sync to server time over 1000ms
 ```
 
 ## Info
@@ -40,8 +48,10 @@ This library is useful for:
 - time-travel debugging
 - animations
 - any situation where you want a version of time that's flowing slower/faster/reversed compared to the actual time
+- keeping a front-end time in sync with a backend time
+- real-time games
 
-We use it for time-travel debugging in our animations library [`redux-time`](https://monadical-sas.github.io/redux-time/).
+We use it for time-travel debugging and animation timing sync in our library [`redux-time`](https://monadical-sas.github.io/redux-time/).
 
 ---
 <img src="examples/static/jeremy.jpg" height="40px" style="float:right"/>
--- src/main.js
+++ src/main.js
@@ -11,15 +11,28 @@
 */
 
 import {select, time} from './reducers.js'
-import {TimeControls, TimeControlsComponent} from './controls.js'
+import {TimeControls, TimeControlsComponent, ServerTimeControls} from './controls.js'
+
 
 class WarpedTime {
-    constructor(store=null, speed=1) {
+    constructor(store=null, speed, server_time, warped_time) {
+        this.store = store
+        this.speed = 1
         this._lastTime = (new Date).getTime()
         this._currTime = this._lastTime
-        this.speed = speed
+        this._server_offset = 0
+
+        if (speed !== undefined) {
+            this.setSpeed(speed)
+        }
+        if (server_time !== undefined) {
+            this.setActualTime(server_time)
+        }
+        if (warped_time !== undefined) {
+            this.setWarpedTime(warped_time)
+        }
+
         if (store) {
-            this.store = store
             this.store.subscribe(this.handleStateChange.bind(this))
         }
     }
@@ -28,15 +41,55 @@ class WarpedTime {
         this.speed = speed
     }
 
-    getWarpedTime(timestamp) {
-        const actualTime = timestamp === undefined ? this.getActualTime() : timestamp
+    getSystemTime() {
+        return (new Date).getTime()
+    }
+
+    getActualTime() {
+        return this.getSystemTime() + this._server_offset
+    }
+
+    setActualTime(server_time, duration) {
+        const system_time = this.getSystemTime()
+        const final_offset = server_time - system_time
+
+        if (duration) {
+            debugger
+            // TODO: test this gradual adjustment code
+            const step_time = 10
+            const total_steps = duration/step_time
+            const step_amt = (final_offset - this._server_offset)/total_steps
+            let step = 0
+            const adjuster = () => {
+                this._server_offset += step_amt
+                step += 1
+                if (step < total_steps && this._server_offset != final_offset) {
+                    setTimemout(adjuster, 10)
+                }
+            }
+            return this.getActualTime()
+        } else {
+            this._server_offset = final_offset
+            return this.getActualTime()
+        }
+    }
+
+    getWarpedTime() {
+        const actualTime = this.getActualTime()
         this._currTime += (actualTime - this._lastTime) * this.speed
         this._lastTime = actualTime
         return this._currTime
     }
 
-    getActualTime() {
-        return (new Date).getTime()
+    setWarpedTime(timestamp, duration) {
+        if (duration) {
+            // TODO: gradual syncing not implemented yet
+            debugger
+        } else {
+            this._lastTime = timestamp
+            this._curTime = this.getActualTime()
+            return this.getWarpedTime()
+        }
     }
 
     handleStateChange() {
