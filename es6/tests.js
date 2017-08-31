5a56aa9726422bb51f8e6d740cdda12af58d5c54
Max <max@monadical.com>
2017-08-30 21:24:38 -0400

    tests with real time values

--- es6/tests.js
+++ es6/tests.js
@@ -1,5 +1,6 @@
 import {WarpedTime} from './main.js'
 
+// util functions
 const assert = (val, error_msg) => {
     if (!val) {
         console.log(`[X] AssertionError: ${error_msg} (${val})`)
@@ -8,26 +9,69 @@ const assert = (val, error_msg) => {
     }
 }
 
+const assert_fuzzy_equal_time = (val1, val2, error_msg) => {
+    assert(Math.abs(val1 - val2) < 25, error_msg)
+}
+
+
+// Tests with a real time source
 const start_time = Date.now()
+const time_passed = () => Date.now() - start_time
+
+const wt1 = new WarpedTime({})
+
+assert_fuzzy_equal_time(wt1.getWarpedTime(), Date.now(),
+                        'wt.getWarpedTime() - Date.now() == '
+                        + (wt1.getWarpedTime() - Date.now())
+                        + ' but should be ~equal')
+
+
+wt1.setSpeed(0.1)
+setTimeout(
+    () => assert_fuzzy_equal_time(wt1.getWarpedTime(), start_time + time_passed() / 10,
+                                  'wt.getWarpedTime() - start_time + time_passed / 10 == '
+                                  + (wt1.getWarpedTime() - start_time + time_passed() / 10)
+                                  + ' but should be ~equal.'
+                                  + ' start_time=' + start_time
+                                  + ' time_passed=' + time_passed())
+    , 100
+)
+
+const wt2 = new WarpedTime({})
+const t = 500
+wt2.setWarpedTime(t)
+const speed = 5
+wt2.setSpeed(speed)
+const delay = 100
+setTimeout(
+    () => assert_fuzzy_equal_time(wt2.getWarpedTime(), t + (delay * speed),
+                                  'wt.getWarpedTime() - t + (delay * speed) == '
+                                  + (wt2.getWarpedTime() - t + (delay * speed))
+                                  + ' but should be ~equal')
+    , 100
+)
+
+
+// Tests with time_source mocked out
 const time_source_mock = {
     now: () => 100
 }
 
-const wt = new WarpedTime({
+const wt3 = new WarpedTime({
     timeSource: time_source_mock,
 })
 
-wt.setSpeed(2)
-assert(wt.speed === 2, 'expected 2, got ' + wt.speed)
+wt3.setSpeed(2)
+assert(wt3.speed === 2, 'expected 2, got ' + wt3.speed)
 
-wt.setWarpedTime(5)
-assert(wt.getWarpedTime() === 5, 'expected 5, got ' + wt.getWarpedTime())
+wt3.setWarpedTime(5)
+assert(wt3.getWarpedTime() === 5, 'expected 5, got ' + wt3.getWarpedTime())
 
 time_source_mock.now = () => 110
 
 // should be 2 * 10 + 5
-assert(wt.getWarpedTime() === 25, 'expected 25, got ' + wt.getWarpedTime())
+assert(wt3.getWarpedTime() === 25, 'expected 25, got ' + wt3.getWarpedTime())
 
 
-wt.setActualTime(115)
-assert(wt.getActualTime() === 115, 'expected 115, got ' + wt.getActualTime())
+wt3.setActualTime(115)
+assert(wt3.getActualTime() === 115, 'expected 115, got ' + wt3.getActualTime())
