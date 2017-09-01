7f15eb809413c5cd9dc4eb6912dba7c1ef5db2f8
Max <max@monadical.com>
2017-09-01 01:43:05 -0400

    SET_WARPED_TIME added to WarpedTime reducer

--- es6/reducers.js
+++ es6/reducers.js
@@ -2,12 +2,15 @@ export const select = (state) => state.time
 
 const initial_state = {
     speed: 1,
+    warped_time: Date.now(),
 }
 
 export const time = (state=initial_state, action) => {
     switch (action.type) {
         case 'SET_SPEED':
             return {...state, speed: action.speed}
+        case 'SET_WARPED_TIME':
+            return {...state, warped_time: action.warped_time}
         default:
             return state
     }
