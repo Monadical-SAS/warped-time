8b54b4930933d6965aa4ec10659c1efc18c77674
Max <max@monadical.com>
2017-09-01 01:43:26 -0400

    update demo page

--- examples/demo.js
+++ examples/demo.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {WarpedTime, TimeControls} from '../node/main.js'
+import {WarpedTime, Tick, TimeControls} from '../node/main.js'
 
+console.log(Tick)
 window.time = new WarpedTime()
+window.tick = new Tick()
 
 ReactDOM.render(
-    <TimeControls time={window.time} debug expanded/>,
+    <TimeControls time={window.time} tick={window.tick} debug expanded/>,
     document.getElementById('react'),
 )
